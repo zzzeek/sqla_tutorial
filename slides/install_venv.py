5019900c6599292ac3882c1e8a894b642e99862e
Mike Bayer <mike_mp@zzzcomputing.com>
2013-09-12 17:08:05 -0400

    unescape slashes

--- slides/install_venv.py
+++ slides/install_venv.py
@@ -10,8 +10,8 @@ import os
 win32 = sys.platform.startswith('win')
 
 if win32:
-    os.system("%s virtualenv-1.10.1\virtualenv.py .venv" % sys.executable)
-    os.system(".venv\Scripts\python.exe install.py")
+    os.system(r"%s virtualenv-1.10.1\virtualenv.py .venv" % sys.executable)
+    os.system(r".venv\Scripts\python.exe install.py")
 else:
     os.system("%s virtualenv-1.10.1/virtualenv.py .venv" % sys.executable)
     os.system(".venv/bin/python install.py")
